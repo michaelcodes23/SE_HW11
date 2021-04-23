9ae274f4d4bd225cd136a22d36b128eaeca1eafa
Michael <cuencamich@hotmail.com>
2021-04-23 10:26:12 -0400

    Finished setting up my schema

---
+++ mongooseVampires/app.js
@@ -0,0 +1,38 @@
+/**********************************************************************
+Set up and Configuration
+**********************************************************************/
+// Dependencies
+const mongoose = require('mongoose');
+const seedData = require('./models/seed_vampires.js');
+const Vampire = require('./models/vampire.js');
+
+// Configuration
+const mongoURI = 'mongodb://localhost:27017/'+ 'vampires';
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect( mongoURI );
+
+// Connection Error/Success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+db.on( 'open' , ()=>{
+  console.log('Connection made!');
+});
+
+/**********************************************************************
+Write Your Code Below
+**********************************************************************/
+var vampire = {
+  name: 'Chocula',
+  title: 'Count',
+  hair_color: 'brown',
+  eye_color: 'brown',
+  dob: new Date(1971, 2, 13, 7, 47),
+  loves: ['cereal', 'marshmallows'],
+  location: 'Minneapolis, Minnesota, US',
+  gender: 'm',
+  victims: 2
+}
---
+++ mongooseVampires/models/vampire.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const vampireSchema = new Schema({
+  // add your code here to set up your schema
+  name: {type: String, required: true, unique: true}
+  hair_color: {type: String, default: "blonde"}
+  eye_color: String,
+  dob: {type: Date, default: Date.now},
+  loves: [{type: String}],
+  location: String,
+  gender: String,
+  victims: {type: Number, min: [0, 'No vampire has less than 0 victims!']}
+}{timestamps: true});
+
+const Vampire = mongoose.model('Vampire', vampireSchema);
+
+//make this exportable to be accessed in `app.js`
+module.exports = Vampire;
