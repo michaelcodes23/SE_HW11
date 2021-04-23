6891b82638a41245e8eb0c698ab758bd9a41a33a
Michael <cuencamich@hotmail.com>
2021-04-23 11:04:30 -0400

    Inserted Seed Data Using Mongoose

--- mongooseVampires/app.js
+++ mongooseVampires/app.js
@@ -25,14 +25,66 @@ db.on( 'open' , ()=>{
 /**********************************************************************
 Write Your Code Below
 **********************************************************************/
-var vampire = {
-  name: 'Chocula',
-  title: 'Count',
-  hair_color: 'brown',
-  eye_color: 'brown',
-  dob: new Date(1971, 2, 13, 7, 47),
-  loves: ['cereal', 'marshmallows'],
-  location: 'Minneapolis, Minnesota, US',
-  gender: 'm',
-  victims: 2
+// var vampire = {
+//   name: 'Chocula',
+//   title: 'Count',
+//   hair_color: 'brown',
+//   eye_color: 'brown',
+//   dob: new Date(1971, 2, 13, 7, 47),
+//   loves: ['cereal', 'marshmallows'],
+//   location: 'Minneapolis, Minnesota, US',
+//   gender: 'm',
+//   victims: 2
+// }
+//Commenting out to avoid uploading the data twice
+// Vampire.insertMany(seedData, (err, vampires) => {
+//   if (err) {console.log(err)}
+//   console.log("added provided vampire data", vampires)
+//   mongoose.connection.close();
+// });
+
+let michael = {
+    name: 'King Michelangelo',
+    hair_color: 'black',
+    eye_color: 'brown',
+    loves: ['Ceviche','Music','Comedies'],
+    location: 'New York, NY, US',
+    gender: 'm',
+    victims: 27
 }
+
+let cindarella = {
+    name: 'Count Cindarella',
+    hair_color: 'black',
+    eye_color: 'blue',
+    loves: ['Dancing','Cleaning','Shoes'],
+    dob: new Date(1578, 10, 8, 5, 3),
+    location: 'Far Far Away',
+    gender: 'f',
+    victims: 1
+}
+
+let charming = {
+    name: 'Prince Charming',
+    eye_color: 'blue',
+    loves: ['Dancing','Investigating','Fighting'],
+    dob: new Date(1576, 10, 8, 5, 3),
+    location: 'Far Far Away',
+    gender: 'm',
+    victims: 0
+}
+
+let now = {
+    name: 'Random',
+    eye_color: 'brown',
+    loves: ['Cooking','Studying','Coding'],
+    location: 'NY',
+    gender: 'f',
+    victims: 50
+}
+
+Vampire.insertMany([michael, cindarella, charming, now], (err, vampires) => {
+  if (err) {console.log(err)}
+  console.log("added provided vampire data", vampires)
+  mongoose.connection.close();
+});
--- mongooseVampires/models/vampire.js
+++ mongooseVampires/models/vampire.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
   // add your code here to set up your schema
-  name: {type: String, required: true, unique: true}
-  hair_color: {type: String, default: "blonde"}
+  name: {type: String, required: true, unique: true},
+  hair_color: {type: String, default: 'Blonde'},
   eye_color: String,
-  dob: {type: Date, default: Date.now},
+  dob: {type: Date, default: Date.now()},
   loves: [{type: String}],
   location: String,
   gender: String,
   victims: {type: Number, min: [0, 'No vampire has less than 0 victims!']}
-}{timestamps: true});
+},{timestamps: true});
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
 
